b8709096258cfbd076ee1f4ec46f192af8aa6cfe
Dirk Geurs <dirk@driebit.nl>
2017-04-25 12:54:29 +0200

    Updated to new versions

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,7 @@
-{:user {:dependencies [[org.clojure/tools.namespace "0.2.8"]
-                       [potemkin "0.3.11"]
+{:user {:dependencies [[org.clojure/tools.namespace "0.2.11"]
+                       [potemkin "0.4.3"]
                        [alembic "0.3.2"]
-                       [criterium "0.4.3"]]
-        :plugins [[lein-try "0.4.3"]
-                  [lein-pprint "1.1.1"]]
-        :repl-options { :init-ns user }
+                       [criterium "0.4.4"]]
+        :plugins [[lein-pprint "1.1.2"]]
+        :repl-options { :init-ns user}
         :source-paths [#=(eval (str (System/getProperty "user.home") "/.lein/src"))]}}
-        
